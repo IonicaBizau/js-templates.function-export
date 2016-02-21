c317cdf738f10e29e4a6245934fb7cf206a99c0a
Ionică Bizău <bizauionica@gmail.com>
2016-02-21 21:04:32 +0200

    Initial

---
+++ example/index.js
@@ -0,0 +1,35 @@
+const functionExport = require("../lib");
+
+console.log(functionExport("foo"));
+// "use strict"
+//
+// /**
+//  * foo
+//  * Sample description
+//  *
+//  * @name foo
+//  * @function
+//  * @param {Number} a Param descrpition.
+//  * @param {Number} b Param descrpition.
+//  * @return {Number} Return description.
+//  */
+// module.exports = function foo (a, b) {
+//     return a + b;
+// };
+
+console.log(functionExport("bar", "Function description"));
+// "use strict"
+//
+// /**
+//  * bar
+//  * Function description
+//  *
+//  * @name bar
+//  * @function
+//  * @param {Number} a Param descrpition.
+//  * @param {Number} b Param descrpition.
+//  * @return {Number} Return description.
+//  */
+// module.exports = function bar (a, b) {
+//     return a + b;
+// };
